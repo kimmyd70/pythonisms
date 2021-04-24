8513b67fec0725d1b92a95709ae1fca40370b626
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-04-24 15:20:05 -0700

    2 class iterators written

---
+++ pythonisms/pythonisms.py
@@ -0,0 +1,57 @@
+# Exploring functionality specific to Python
+
+# iterators/generators
+
+# basic iterator class with limit (help from G4G):
+class Print:
+    def __init__(self, limit):
+        self.limit = limit
+        
+    def __iter__(self):
+        self.x = 1
+        return self
+
+    def __next__(self):
+        count = self.x
+        
+        if count <= self.limit:
+            self.x = count + 1
+            return count
+        else:
+            raise StopIteration
+
+
+class Printcount:
+    def __init__(self, limit):
+        self.limit = limit
+        
+    def __iter__(self):
+        self.x = 1
+        return self
+
+    def __next__(self):
+        count = self.x
+        
+        if count <= self.limit:
+            self.x = count + 2
+            return count
+        else:
+            raise StopIteration
+
+# decorators
+
+
+# dunders
+
+
+# "Anything that catches your eye"
+
+# *args and *kwargs -- useful for writing functions with generic parameters when
+# arguments are unknown--works brilliantly with multiple passed-in arguments of 
+# different types and using built-in .join():
+
+def generic_arguments(*args):
+    for item in args:
+        return " ".join(args)
+    
+    
--- tests/test_pythonisms.py
+++ tests/test_pythonisms.py
@@ -1,5 +1,40 @@
 from pythonisms import __version__
+import pytest
+from pythonisms.pythonisms import  Print, Printcount, generic_arguments
 
-
+# passes -- proof of life with poetry new
+@pytest.mark.skip('Passed On Previous Run')
 def test_version():
     assert __version__ == '0.1.0'
+
+# 2 passes -- iterator/generator tests
+@pytest.mark.skip('Passed On Previous Run')
+def test_print_count():
+    result = []
+    for i in Print(3):
+        result.append(i)
+    
+    expected = [1,2,3]
+    assert result == expected
+
+@pytest.mark.skip('Passed On Previous Run')    
+def test_print_odds():
+    result = []
+    for i in Printcount(8):
+        result.append(i)
+    expected = [1,3,5,7]
+    assert result == expected
+        
+    
+
+    
+# passes -- *args with different types and .join()
+# @pytest.mark.skip('Passed On Previous Run')
+def test_args():
+    a = 'Aliya'
+    b = ['is', 'awesome']
+    c = ('!', )
+    
+    actual = generic_arguments(*a, *b, *c)
+    expected = 'A l i y a is awesome !'
+    assert actual == expected
